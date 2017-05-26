1d7b5c9fbb31aba713adc2e51e9d87e590c41986
Louis Segretin <Louis.Segretin@grenoble-inp.org>
2017-05-26 15:30:50 +0200

    ajout de la structure du projet

---
+++ projet_c_5620/src/ei_application.c
@@ -0,0 +1,87 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "ei_types.h"
+#include "hw_interface.h"
+#include "ei_draw.h"
+#include "ei_application.h"
+
+
+/**
+ * \brief	Creates an application.
+ *		<ul>
+ *			<li> initializes the hardware (calls \ref hw_init), </li>
+ *			<li> registers all classes of widget and all geometry managers, </li>
+ *			<li> creates the root window (either in a system window, or the entire
+ *				screen), </li>
+ *			<li> creates the root widget to accress the root window. </li>
+ *		</ul>
+ *
+ * @param	main_window_size	If "fullscreen is false, the size of the root window of the
+ *					application.
+ *					If "fullscreen" is true, the current monitor resolution is
+ *					used as the size of the root window, this size is returned
+ *					in this parameter.
+ * @param	fullScreen		If true, the root window is the entire screen. Otherwise, it
+ *					is a system window.
+ */
+void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen){
+    // Init access to hardware
+    hw_init();
+
+    //Create the main window
+	main_window = hw_create_window(main_window_size, fullscreen);
+
+    //Draws background
+    ei_fill(main_window, NULL, NULL);
+}
+
+/**
+ * \brief	Releases all the resources of the application, and releases the hardware
+ *		(ie. calls \ref hw_quit).
+ */
+void ei_app_free(){
+    hw_exit();
+
+}
+
+
+/**
+ * \brief	Runs the application: enters the main event loop. Exits when
+ *		\ref ei_app_quit_request is called.
+ */
+void ei_app_run(){
+    getchar();
+}
+
+/**
+ * \brief	Adds a rectangle to the list of rectangles that must be updated on screen. The real
+ *		update on the screen will be done at the right moment in the main loop.
+ *
+ * @param	rect		The rectangle to add, expressed in the root window coordinates.
+ *				A copy is made, so it is safe to release the rectangle on return.
+ */
+void ei_app_invalidate_rect(ei_rect_t* rect){
+
+}
+
+/**
+ * \brief	Tells the application to quite. Is usually called by an event handler (for example
+ *		when pressing the "Escape" key).
+ */
+void ei_app_quit_request(){
+
+}
+
+/**
+ * \brief	Returns the "root widget" of the application: a "frame" widget that encapsulate the
+ *		root window.
+ *
+ * @return 			The root widget.
+ */
+ei_widget_t* ei_app_root_widget(){
+
+}
---
+++ projet_c_5620/src/ei_event.c
@@ -0,0 +1,60 @@
+#include "ei_event.h"
+/**
+ *  @file	ei_event.c
+ *  @brief	Allows the binding and unbinding of callbacks to events.
+ *
+ *  \author 
+ *  Created by François Bérard on 30.12.11.
+ *  Copyright 2011 Ensimag. All rights reserved.
+ *
+ */
+
+/**
+ * Sets the widget which is currently being manipulated by the user.
+ *
+ * @param	widget		The widget to declare as active, or NULL to declare
+ *				that a widget is no more being manipulated.
+ */
+void ei_event_set_active_widget(ei_widget_t* widget){
+}
+
+/**
+ * Returns the widget currently being manipulated by the user.
+ *
+ * @return			The widget currenlty being manipulated, or NULL.
+ */
+ei_widget_t* ei_event_get_active_widget(){
+}
+
+
+
+/**
+ * @brief	A function that is called in response to an event that has not been processed
+ *		by any widget.
+ *
+ * @param	event		The event containing all its parameters (type, etc.)
+ *
+ * @return			EI_TRUE if the function handled the event,
+ *				EI_FALSE otherwise, in this case the event is dismissed.
+ */
+typedef ei_bool_t		(*ei_default_handle_func_t)(struct ei_event_t* event){
+}
+
+/**
+ * Sets the function that must be called when an event has been received but no processed
+ *	by any widget.
+ *
+ * @param	func		The event handling function.
+ */
+void ei_event_set_default_handle_func(ei_default_handle_func_t func){
+}
+
+/**
+ * Returns the function that must be called when an event has been received but no processed
+ *	by any widget.
+ *
+ * @return			The address of the event handling function.
+ */
+ei_default_handle_func_t ei_event_get_default_handle_func(){
+}
+
